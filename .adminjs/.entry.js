275e2df7d3da114ba87e3e074f16c6a2c41f05e7
Natanael Carvalho <natanaelc7@gmail.com>
2023-08-17 16:37:17 -0300

    2.Configurando o Sequelize

---
+++ .adminjs/.entry.js
@@ -0,0 +1,13 @@
+AdminJS.UserComponents = {}
+import Component1 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+AdminJS.UserComponents.Component1 = Component1
+import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
+AdminJS.UserComponents.Component2 = Component2
+import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
+AdminJS.UserComponents.Component3 = Component3
+import Component4 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+AdminJS.UserComponents.Component4 = Component4
+import Component5 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
+AdminJS.UserComponents.Component5 = Component5
+import Component6 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
+AdminJS.UserComponents.Component6 = Component6
