c027b2047c158f4a360edd806caf56157cb65b0e
Natanael Carvalho <natanaelc7@gmail.com>
2023-08-23 12:03:17 -0300

    Incluindo paginação de categorias

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginetionParams";
 
 export const categoriesController = {
     index: async (req: Request, res: Response ) => {
-        try {
-            const categories = await Category.findAll({
-                attributes: ['id', 'name', 'position'],
-                order: [['position', 'ASC']]
-            })
+        const [page, perPage] = getPaginationParams(req.query)
 
-            return res.json(categories)
+        try {
+            const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+
+            return res.json(paginatedCategories)
+
         } catch (err) {
             if (err instanceof Error){
                 return res.status(400).json({ message: err.message})
             }
-        }
-        
-        
+        }     
     }
 }
