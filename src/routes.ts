30dcba05810e1fe60e7c6d11861274bccffcccdf
Natanael Carvalho <natanaelc7@gmail.com>
2023-08-24 12:18:02 -0300

    Obtendo 3 cursos em destaque

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesController";
 
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
+
+router.get('/courses/featured', coursesController.featured)
+router.get('/courses/:id', coursesController.show)
 
 export {router}
---
+++ src/services/courseService.ts
@@ -0,0 +1,47 @@
+import { Course } from "../models"
+
+export const courseService = {
+    findByIdWithEpisodes: async (id: string) => {
+        const courseWithEpisodes = await Course.findByPk(id,{
+            attributes:[
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']
+            ],
+            include: {
+                association: 'episodes',
+                attributes: [
+                    'id',
+                    'name',
+                    'synopsis',
+                    'order',
+                    ['video_url', 'videoUrl'],
+                    ['seconds_long', 'secondsLong']
+                ],
+                order: [['order', 'ASC']],
+                separate: true
+            }
+        })
+
+        return courseWithEpisodes
+    },
+
+    getRandomFeaturedCourses: async () => {
+        const featuredCourses = await Course.findAll({
+            attributes:[
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']
+            ],
+            where: {
+                featured: true
+            }
+        })
+
+        const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+
+        return randomFeaturedCourses.slice(0, 3)
+    }
+}
