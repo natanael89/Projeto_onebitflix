9419964c91018023ddf53139ce83abeeb1691b52
Natanael Carvalho <natanaelc7@gmail.com>
2023-08-24 13:10:34 -0300

    Obtendo os lançamentos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -15,6 +15,19 @@ export const coursesController = {
         }
     },
 
+    // GET /courses/newest
+    newest: async (req: Request, res: Response) => {
+        
+        try {
+            const newestCourses = await courseService.getTopTenNewest()
+            return res.json(newestCourses)
+        } catch (err){
+            if (err instanceof Error){
+                return res.status(400).json({ message: err.message})
+            }
+        }
+    },
+
 
     // GET /courses/:id
     show: async (req: Request, res: Response) => {
