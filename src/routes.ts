dee801ef4d94266a89e72f8bdd4fb74955e17c90
Natanael Carvalho <natanaelc7@gmail.com>
2023-08-25 12:55:20 -0300

    Middleware de autorização

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
+import { ensureAuth } from "./middlewares/auth";
 
 const router = express.Router()
 
@@ -11,14 +12,14 @@ router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
 // categories
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
+router.get('/categories', ensureAuth, categoriesController.index)
+router.get('/categories/:id', ensureAuth, categoriesController.show)
 
 // courses
-router.get('/courses/featured', coursesController.featured)
+router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id', coursesController.show)
+router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/:id', ensureAuth, coursesController.show)
 
 // episodes
 router.get('/episodes/stream', episodesController.stream)
--- src/services/jwtServece.ts
+++ src/services/jwtServece.ts
@@ -3,9 +3,15 @@ import jwt from 'jsonwebtoken'
 const secret = 'chave-do-jwt'
 
 export const jwtService = {
+    // Receber Token
     signtToken: (payload: string | object | Buffer, expiration: string) => {
         return jwt.sign(payload, secret, {
             expiresIn: expiration
         })
+    },
+
+    // Verificar Token
+    verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+        jwt.verify(token, secret, callbackfn)
     }
 }
