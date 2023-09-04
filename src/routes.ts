4c70b12a8daa49f489e2286092603320fb166763
Natanael Carvalho <natanaelc7@gmail.com>
2023-09-04 16:45:29 -0300

    Obtendo informações da conta de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -41,6 +41,7 @@ router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
 
 // lista de continuação
+router.get('/users/current', ensureAuth, usersController.show)
 router.get('/users/current/watching', ensureAuth, usersController.watching)
 
 export {router}
