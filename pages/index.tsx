435012600324f50d7a18a1ae7aba22632de5a7cc
Natanael Carvalho <natanaelc7@gmail.com>
2023-09-15 15:30:04 -0300

    Intalação do AOS, Ultilização do AOS na homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ import CardsSection from "../src/components/homeNoAuth/cardsSection";
 import SlideSection from "../src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "@/src/components/common/footer";
+import AOS  from "aos";
+import "aos/dist/aos.css";
 
 
 interface IndexPageProps {
@@ -16,6 +18,10 @@ interface IndexPageProps {
 }
 
 const HomeNoAuth = ({course}: IndexPageProps) => {
+  useEffect(() => {
+     AOS.init();
+  }, []);
+
   return (
     <>
        <Head>
@@ -25,13 +31,19 @@ const HomeNoAuth = ({course}: IndexPageProps) => {
         <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
        </Head>
        <main>
-        <div className={styles.sectionBackground}>
+        <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
            <HeaderNoAuth/>
            <PresentationSection/>
           
         </div>
-           <CardsSection/>
-           <SlideSection newestCourses={course}/>
+        <div data-aos="fade-right" data-aos-duration="1200">
+        <CardsSection/>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+         <SlideSection newestCourses={course}/>
+        </div>
+           
+          
            <Footer/>
        </main>
     </>
