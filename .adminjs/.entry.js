546550ab375cdace3e23273943d6bea5235bf33d
unknown <natanaelc7@gmail.com>
2023-11-15 11:56:42 -0300

    Deploy

--- .adminjs/.entry.js
+++ .adminjs/.entry.js
@@ -1,15 +1,15 @@
 AdminJS.UserComponents = {}
-import Component1 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+import Component0 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+AdminJS.UserComponents.Component0 = Component0
+import Component1 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
 AdminJS.UserComponents.Component1 = Component1
-import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
+import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
 AdminJS.UserComponents.Component2 = Component2
-import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
+import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
 AdminJS.UserComponents.Component3 = Component3
-import Component4 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+import Component4 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
 AdminJS.UserComponents.Component4 = Component4
-import Component5 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
+import Component5 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
 AdminJS.UserComponents.Component5 = Component5
-import Component6 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
+import Component6 from '../src/adminjs/components/Dashboard'
 AdminJS.UserComponents.Component6 = Component6
-import Component7 from '../src/adminjs/components/Dashboard'
-AdminJS.UserComponents.Component7 = Component7
